0745541d58a81be7e191060c4aa2a65ec3047bc7
FelipeHiroshi13 <felipehiroshi13@gmail.com>
2026-03-10 15:43:24 -0400

    feat: implementado slivers

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
-import 'package:app_settings/provider/AppSettingsProvider.dart';
-import 'package:datastore/provider/session/session_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:injectable/injectable.dart';
-import 'package:login/login.dart';
 
-import 'di/injection.dart';
-
- Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
- await configureDependencies(Environment.dev);
   runApp(const MyApp());
 }
 
@@ -19,26 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -47,15 +21,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -63,73 +28,252 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    doLogin();
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            toolbarHeight: 100,
+            backgroundColor: Colors.white,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      spacing: 4,
+                      children: [
+                        Text(
+                          'Welcome back!',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                        Text(
+                          'John Joe',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.notifications_outlined),
+                      onPressed: () {},
+                      iconSize: 28,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverAppBar(
+            expandedHeight: 200,
+            backgroundColor: Colors.deepPurple,
+            floating: true,
+            flexibleSpace: FlexibleSpaceBar(
+              titlePadding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              background: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '\$5,270.00',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Current Balance',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              title: Text(
+                'Wallet',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            expandedHeight: 170,
+            toolbarHeight: 170,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 10,
+                  children: [
+                    Text(
+                      'Quick actions',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        AvatarTile(icon: Icons.send, label: 'Send'),
+                        AvatarTile(icon: Icons.receipt, label: 'label'),
+                        AvatarTile(icon: Icons.add, label: 'label'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                'Recent Transactions',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 20,
+              (context, index) {
+                return QuickActionCard(
+                  title: 'Transaction $index',
+                  amount: (index.isEven ? '+' : '-'),
+                  subtitle: '',
+                  isPositive: index.isEven,
+                );
+              },
+            ),
+          ),
+          SliverFillRemaining(
+            child: Column(
+              spacing: 16,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.lightbulb_outline,
+                  size: 64,
+                  color: Colors.deepPurple,
+                ),
+                Text(
+                  'Explore New Features',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Stay tunned for exciting updates',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
+}
+
+class AvatarTile extends StatelessWidget {
+  const AvatarTile({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
+
+  final IconData icon;
+  final String label;
 
   @override
   Widget build(BuildContext context) {
-    final appSettingsProvider = getIt<AppSettingsProvider>();
-    final sessionProvider = getIt<SessionProvider>();
+    return Column(
+      spacing: 8,
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundColor: Colors.deepPurple.withValues(alpha: .4),
+          child: Icon(
+            icon,
+            color: Colors.deepPurple,
+            size: 24,
+          ),
+        ),
+        Text(
+          label,
+          style: TextStyle(fontSize: 14),
+        )
+      ],
+    );
+  }
+}
 
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'App Language: ${appSettingsProvider.getAppLanguage()}',
-            ),
-            Text(
-              'App Theme: ${appSettingsProvider.getThemeType()}',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              'Session ClientId: ${sessionProvider.getClientId()}',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+class QuickActionCard extends StatelessWidget {
+  const QuickActionCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.amount,
+    required this.isPositive,
+  });
+
+  final String title;
+  final String subtitle;
+  final String amount;
+  final bool isPositive;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: ListTile(
+        leading: CircleAvatar(),
+        title: Text(
+          title,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(subtitle),
+        trailing: Text(
+          amount,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: isPositive ? Colors.green : Colors.red,
+          ),
+        ),
+      ),
     );
   }
 }
